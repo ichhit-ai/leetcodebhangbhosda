79c753e4aec98766d0d47b7658f5b21a9990a0e0
ichhit karwa <ichhit.24bce11364@vitbhopal.ac.in>
2026-08-08 00:22:55 +0530

    1408-FIND-THE-SMALLEST-DIVISOR-GIVEN-A-THRESHOLD Stats: Time: 36 ms (6.72%), Space: 51.2 MB (34.66%) - LeetHub

---
+++ 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.java
@@ -0,0 +1,34 @@
+class Solution {
+    public long thresholding(int[] nums, int divisor) {
+        long threshy = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            threshy += (int) Math.ceil((double) nums[i] / divisor);
+        }
+
+        return threshy;
+    }
+
+    public int smallestDivisor(int[] nums, int threshold) {
+        long sum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum = sum + nums[i];
+        }
+
+        long low = 1;
+        long high = sum;
+
+        while (low <= high) {
+            long mid = low + (high - low) / 2;
+
+            if (thresholding(nums, (int) mid) <= threshold) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return (int) low;
+    }
+}
--- README.md
+++ README.md
@@ -6,9 +6,11 @@ A collection of LeetCode questions to ace the coding interview! - Created using 
 ## Array
 |  |
 | ------- |
+| [1408-find-the-smallest-divisor-given-a-threshold](https://github.com/ichhit-ai/leetcodebhangbhosda/tree/master/1408-find-the-smallest-divisor-given-a-threshold) |
 | [1886-minimum-limit-of-balls-in-a-bag](https://github.com/ichhit-ai/leetcodebhangbhosda/tree/master/1886-minimum-limit-of-balls-in-a-bag) |
 ## Binary Search
 |  |
 | ------- |
+| [1408-find-the-smallest-divisor-given-a-threshold](https://github.com/ichhit-ai/leetcodebhangbhosda/tree/master/1408-find-the-smallest-divisor-given-a-threshold) |
 | [1886-minimum-limit-of-balls-in-a-bag](https://github.com/ichhit-ai/leetcodebhangbhosda/tree/master/1886-minimum-limit-of-balls-in-a-bag) |
 <!---LeetCode Topics End-->
