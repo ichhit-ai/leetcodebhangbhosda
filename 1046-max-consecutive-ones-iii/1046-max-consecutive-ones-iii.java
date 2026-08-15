7b22a87f3b6a2d8245e11a2da45a0fec78592223
ichhit karwa <ichhit.24bce11364@vitbhopal.ac.in>
2026-08-16 00:07:32 +0530

    1046-MAX-CONSECUTIVE-ONES-III Stats: Time: 3 ms (95.8%), Space: 52.4 MB (18.08%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
@@ -1,9 +1,9 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
         int count = 0;
-        int maxcount  = 0;
         int x = k;
-        int right = 0;
+        
+        
         int left = 0;
         for(int i = 0; i<k; i++){
             if(nums[i] == 1){
@@ -13,7 +13,7 @@ class Solution {
                 count++;
                 x--;
             }
-            maxcount  = count;
+          
         }
         
         for(int i = k; i<nums.length;i++){
