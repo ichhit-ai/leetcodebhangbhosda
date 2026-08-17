73d2ce3e6a0bc3106e0c50d08902c7a5232be8af
ichhit karwa <ichhit.24bce11364@vitbhopal.ac.in>
2026-08-17 21:37:40 +0530

    0713-SUBARRAY-PRODUCT-LESS-THAN-K Stats: Time: 2 ms (99.96%), Space: 49.1 MB (32.68%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
@@ -1,29 +1,26 @@
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
         int count = 0;
-        int multiple = 0;
+        int multiple = 1;
+        int left = 0;
         int right = 0;
 
         if (k <= 1) {
             return 0;
         }
 
-        for (int i = 0; i < nums.length; i++) {
-            multiple = nums[i];
-            right = i + 1;
+        while (right < nums.length) {
+            multiple = multiple * nums[right];
 
-            if (multiple < k) {
-                count++;
+            while (multiple >= k) {
+                multiple = multiple / nums[left];
+                left++;
             }
-            while (right <= nums.length - 1 && multiple < k) {
-                multiple = multiple * nums[right];
 
-                if (multiple < k) {
-                    count++;
-                    right++;
-                }
-            }
+            count = count + right - left + 1;
+            right++;
         }
+
         return count;
     }
 }
