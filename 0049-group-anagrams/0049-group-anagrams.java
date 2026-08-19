1ad5b7d3d2f4f58ad62e3d5378c650baa6b59a8a
ichhit karwa <ichhit.24bce11364@vitbhopal.ac.in>
2026-08-20 03:36:01 +0530

    0049-GROUP-ANAGRAMS Stats: Time: 19 ms (15.99%), Space: 50.4 MB (13.27%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -3,20 +3,17 @@ class Solution {
         Map<String, List<String>> map = new HashMap<>();
 
         for (String s : strs) {
-            // build a signature: count of each letter, encoded as a string
             int[] count = new int[26];
             for (char c : s.toCharArray()) {
                 count[c - 'a']++;
             }
 
-            // turn the count array into a unique key, e.g. "1#0#0#2#0..." 
             StringBuilder sb = new StringBuilder();
             for (int i = 0; i < 26; i++) {
                 sb.append(count[i]).append('#');
             }
             String key = sb.toString();
 
-            // group strings under the same signature
             map.computeIfAbsent(key, x -> new ArrayList<>()).add(s);
         }
 
