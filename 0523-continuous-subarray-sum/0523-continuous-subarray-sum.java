5a7dbbdbfd6738b520d88ae74b62f2ee616f62b8
ichhit karwa <ichhit.24bce11364@vitbhopal.ac.in>
2026-08-25 05:02:37 +0530

    0523-CONTINUOUS-SUBARRAY-SUM Stats: Time: 22 ms (97.35%), Space: 139.6 MB (77.4%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
@@ -1,25 +1,33 @@
 class Solution {
     public boolean checkSubarraySum(int[] nums, int k) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-
         int[] prefix = new int[nums.length + 1];
 
-        map.put(0, -1);
-
-        for (int i = 0; i < nums.length; i++) {
-
+        for(int i = 0; i < nums.length; i++) {
             prefix[i + 1] = prefix[i] + nums[i];
-            int rem = prefix[i + 1] % k;
+        }
 
-            if (map.containsKey(rem)) {
-                if (i - map.get(rem) >= 2) {
+        Map<Integer, Integer> m = new HashMap<>();
+
+        for(int i = 0; i < prefix.length; i++) {
+            int reminder = prefix[i] % k;
+            if (m.containsKey(reminder)) {
+                if (i - m.get(reminder) >= 2) {
                     return true;
                 }
             } else {
-                map.put(rem, i);
+                m.put(prefix[i] % k, i);
             }
         }
 
         return false;
     }
 }
+/*
+23 2 4 6 7
+0 23 25 29 35 42
+
+sum(i, j) = prefix[j + 1] - prefix[i];
+(prefix[j + 1] - prefix[i]) % k == 0;
+                 <--->
+prefix[j + 1] % k == prefix[i] % k*/
+
