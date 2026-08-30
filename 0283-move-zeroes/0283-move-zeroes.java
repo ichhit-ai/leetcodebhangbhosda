1427e4549fcf28041d404b7980556d7426c23f52
ichhit karwa <ichhit.24bce11364@vitbhopal.ac.in>
2026-08-30 10:05:04 +0530

    0283-MOVE-ZEROES Stats: Time: 2 ms (91.93%), Space: 47.7 MB (76.4%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,18 +1,12 @@
 class Solution {
-
-    public void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-
     public void moveZeroes(int[] nums) {
         int left = 0;
 
         for (int right = 0; right < nums.length; right++) {
-
             if (nums[right] != 0) {
-                swap(nums, left, right);
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
                 left++;
             }
         }
