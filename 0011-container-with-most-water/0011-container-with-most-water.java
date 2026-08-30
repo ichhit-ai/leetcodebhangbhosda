639e21e582d2fb6b70e8500d2669ea10f2e7e3bb
ichhit karwa <ichhit.24bce11364@vitbhopal.ac.in>
2026-08-30 21:42:50 +0530

    0011-CONTAINER-WITH-MOST-WATER Stats: Time: 5 ms (83.18%), Space: 77.3 MB (69.02%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,25 +1,22 @@
 class Solution {
     public int maxArea(int[] height) {
-        int left  = 0;
-        int right  = height.length - 1;
-        int max= 0;
+        int area = 0;
+        int max = 0;
+        int i = 0;
+        int j = height.length - 1;
 
-        while(left<=right){
-            int area = Math.min(height[left], height[right])*(right-left);
-            
-            if(height[right]>=height[left]){
-                left++;
+        while (i < j) {
+            area = Math.min(height[i], height[j]) * (j - i);
+
+            if (height[i] <= height[j]) {
+                i++;
+            } else {
+                j--;
             }
-            else{
-                right--;
-            }
-            if(max<area){
-                max = area;
-            }
-            
+
+            max = Math.max(area, max);
         }
-        return max;
 
-        
+        return max;
     }
 }
