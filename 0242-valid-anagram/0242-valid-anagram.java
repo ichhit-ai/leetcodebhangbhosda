c4436ee3a01ea9e5e788a6fdc136b32b048737a3
ichhit karwa <ichhit.24bce11364@vitbhopal.ac.in>
2026-08-31 19:14:05 +0530

    0242-VALID-ANAGRAM Stats: Time: 26 ms (5.13%), Space: 46.7 MB (9.05%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,23 +1,15 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        HashMap<Character, Integer> maps = new HashMap<>();
-        HashMap<Character, Integer> mapt = new HashMap<>();
-
-        if (s.length() != t.length()) {
+        if(s.length() != t.length()){
             return false;
         }
-
-        for (int i = 0; i < s.length(); i++) {
-            maps.put(s.charAt(i), maps.getOrDefault(s.charAt(i), 0) + 1);
-            mapt.put(t.charAt(i), mapt.getOrDefault(t.charAt(i), 0) + 1);
+        HashMap<Character, Integer> maps = new HashMap<>();
+        HashMap<Character, Integer> mapt = new HashMap<>();
+        for(int i = 0; i<s.length(); i++){
+            maps.put(s.charAt(i), maps.getOrDefault(s.charAt(i), 0) +1);
+            mapt.put(t.charAt(i), mapt.getOrDefault(t.charAt(i), 0) +1);
         }
+        return maps.equals(mapt);
 
-        for (int i = 0; i < s.length(); i++) {
-            if (!maps.get(s.charAt(i)).equals(mapt.get(s.charAt(i)))) {
-                return false;
-            }
-        }
-
-        return true;
     }
 }
